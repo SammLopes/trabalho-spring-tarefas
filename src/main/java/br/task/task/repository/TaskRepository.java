78703b9b9fc3d51bd775af599ea9a21b8fb9cab5
SammLopes <sam.v.p.lopes@gmail.com>
2024-03-23 22:48:10 -0300

    Criadno repository

--- src/main/java/br/task/task/repository/TaskRepository.java
+++ src/main/java/br/task/task/repository/TaskRepository.java
@@ -1,5 +1,9 @@
 package br.task.task.repository;
 
-public class TaskRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.task.task.model.Task;
+
+public interface TaskRepository extends JpaRepository<Task, Long>{
     
 }
