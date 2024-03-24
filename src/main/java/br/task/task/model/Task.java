036e1fd2853830a207acfd1c986d941830d8a96c
SammLopes <sam.v.p.lopes@gmail.com>
2024-03-23 22:41:30 -0300

    Create model de tarefas

--- src/main/java/br/task/task/model/Task.java
+++ src/main/java/br/task/task/model/Task.java
@@ -1,5 +1,25 @@
 package br.task.task.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Task {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String title; 
+    private String description;
+    private Boolean completed = false;
 }
