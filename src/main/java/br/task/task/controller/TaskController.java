2d9cf22de8d9d8b7fd143bf3b4bb8b5452209889
SammLopes <sam.v.p.lopes@gmail.com>
2024-03-23 23:20:09 -0300

    Add method busca de task por id

--- src/main/java/br/task/task/controller/TaskController.java
+++ src/main/java/br/task/task/controller/TaskController.java
@@ -1,5 +1,31 @@
 package br.task.task.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.task.task.model.Task;
+import br.task.task.services.TaskService;
+
+@RestController
+@RequestMapping("gravar/tarefas")
 public class TaskController {
     
+    @Autowired
+    TaskService service;
+
+    @GetMapping("/task/{id}")
+    public Task getTaskById(@PathVariable Long id){
+        
+        Task task = service.getTaskById(id);
+
+        if(task == null){
+            return null;
+        }
+
+        return task;
+
+    }
 }
--- src/main/java/br/task/task/services/TaskService.java
+++ src/main/java/br/task/task/services/TaskService.java
@@ -1,5 +1,25 @@
 package br.task.task.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.task.task.model.Task;
+import br.task.task.repository.TaskRepository;
+
+/**getAllTasks, getTaskById, createTask, updateTask, deleteTask*/ 
+
+@Service
 public class TaskService {
+ 
+    @Autowired
+    TaskRepository repository;
+
+    public Task getTaskById(Long id){
+        Task task = this.repository.findById(id).get();
+        if(task != null){
+            return task;
+        } 
+        return null;
+    }
     
 }
